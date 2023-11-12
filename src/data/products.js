14a2a1e0cfb49bf4a9236fcb9be72f3c9f1c7db7
Eugenio Garcia <eugeniog288@gmail.com>
2023-11-12 20:06:48 -0300

    Se crea la lista de productos

---
+++ src/data/products.js
@@ -0,0 +1,51 @@
+[
+	{
+		"name": "PC Gamer Inicial",
+		"id": 1,
+		"category": "desktop",
+		"detail": "Pc Gamer Ryzen 3 4100 | Rtx 2060 6gb | 16gb Ram | Ssd 480gb | Fuente 550w 80 Plus Bronze",
+		"img": "https://www.megatecnologia.com.ar/images/1699278546842.jpg"
+	},
+	{
+		"name": "PC Gamer Intermedia",
+		"id": 2,
+		"category": "desktop",
+		"detail": "Pc Gamer Intel Core I3 12100f | Rtx 3050 8gb | 16gb Ram | Fuente 550w 80 Plus Bronze",
+		"img": "https://www.megatecnologia.com.ar/images/1698785622458.jpg"
+	},
+	{
+		"name": "PC Gamer Avanzada",
+		"id": 3,
+		"category": "desktop",
+		"detail": "Pc Gamer Amd Ryzen 5 7600x | Rtx 4060 8gb | 16gb Ram | Ssd Nvme M.2 500 Gb | Fuente 750w 80 Plus Bronze",
+		"img": "https://www.megatecnologia.com.ar/images/1699360047392.jpg"
+	},
+    {
+		"name": "Notebook Lenovo 14'",
+		"id": 4,
+		"category": "notebooks",
+		"detail": "Notebook Lenovo 14'' Ip 1 14ada7 R3 3250u | Ram 8gb | Ssd 256gb | Freedos",
+		"img": "https://www.megatecnologia.com.ar/images/1692805801220.jpg"
+	},
+    {
+		"name": "Notebook Lenovo 16.6'",
+		"id": 5,
+		"category": "notebooks",
+		"detail": "Notebook Lenovo 15.6'' Ip 3 15alc6 | Amd Ryzen 5 5500u | 8gb Ram | 256gb Ssd | W11h",
+		"img": "https://www.megatecnologia.com.ar/images/1681930171482.jpg"
+	},
+    {
+		"name": "VIDEO GEFORCE RTX 4060 8GB ASUS DUAL OC",
+		"id": 6,
+		"category": "componentes",
+		"detail": "ASUS Dual GeForce RTX™ 4060 OC Edition 8GB GDDR6 con dos potentes ventiladores Axial-tech y un diseño de 2.5 ranuras para una amplia compatibilidad.",
+		"img": "https://www.fullh4rd.com.ar/img/productos/3/video-geforce-rtx-4060-8gb-asus-dual-oc-0.jpg"
+	},
+    {
+		"name": "VIDEO GEFORCE GTX 1660 SUPER 6GB ASUS TUF GAMING",
+		"id": 7,
+		"category": "componentes",
+		"detail": "ASUS TUF Gaming GeForce® GTX 1660 SUPER™ 6GB GDDR6 ofrece altas frecuencias de actualización sin sudar.",
+		"img": "https://www.fullh4rd.com.ar/img/productos/3/video-geforce-gtx-1660-super-6gb-asus-tuf-gaming-0.jpg"
+	}
+]
