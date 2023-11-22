707f9d8b1288597f31185a3a3666e9c1e3f72ef1
Eugenio Garcia <eugeniog288@gmail.com>
2023-11-22 00:03:15 -0300

    Se corrige el componente NavBar para eliminar el error el warning de consola.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 import { CartWidget } from "./CartWidget";
 import { products } from "../data/products";
 
-const categories = products.map(item => item.category);
-const uniqueCategories = new Set(categories)
+const categories = products.map((item) => item.category);
+const uniqueCategories = new Set(categories);
 
 console.log(uniqueCategories);
 
 export const NavBar = () => {
-	return (
-		<header>
-			<nav>
-				<div className="nav-brand"><Link to={"/"}>DeltaTech</Link></div>
-				<ul>
-					<li><Link to={"/"}>Inicio</Link></li>
-					{[...uniqueCategories].map(item => <li><Link to={`/category/${item}`}>{item}</Link></li>)}
-					<li><Link to={"/contacto"}>Contacto</Link></li>
-				</ul>
-				<CartWidget />
-			</nav>
-		</header>
-	);
-}
+  return (
+    <header>
+      <nav>
+        <div className="nav-brand">
+          <Link to={"/"}>DeltaTech</Link>
+        </div>
+        <ul>
+          <li>
+            <Link to={"/"}>Inicio</Link>
+          </li>
+          {[...uniqueCategories].map((item) => (
+            <li key={item}>
+              <Link to={`/category/${item}`}>{item}</Link>
+            </li>
+          ))}
+          <li>
+            <Link to={"/contacto"}>Contacto</Link>
+          </li>
+        </ul>
+        <CartWidget />
+      </nav>
+    </header>
+  );
+};
