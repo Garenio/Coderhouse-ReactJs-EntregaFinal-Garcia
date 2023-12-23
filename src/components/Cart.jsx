2223423c69da359cda665cffd7e8aeb696708d8c
Eugenio Garcia <eugeniog288@gmail.com>
2023-12-23 11:12:31 -0300

    Se comienza a crear la lógica para el checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,72 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import { CartContext } from "../contexts/CartContext";
 
-export const Cart = () => {
-  const { cartList } = useContext(CartContext);
+const clearBuyer = { name: "", phone: "", email: "" };
 
-  console.log(cartList);
+export const Cart = () => {
+  const [buyer, setBuyer] = useState(clearBuyer);
+  const { clear, cartList } = useContext(CartContext);
+
+  const handleSendOrder = () => {
+    const order = {
+      buyer,
+      cartList,
+      total: 13600,
+    };
+  };
+
+  const handleChange = (ev) => {
+    const { name, value } = ev.target;
+    setBuyer((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
 
   return (
-    <div className="cart-list">
-      {cartList.map((item) => (
-        <div key={item.id} className="product-card">
-          <img src={item.img} alt="Imagen del producto" />
-          <h3>{item.name}</h3>
-          <p>{item.detail}</p>
-        </div>
-      ))}
+    <div className="cart-detail">
+      <div className="cart-products-list">
+        {cartList.map((item) => (
+          <div key={item.id} className="product-card">
+            <img src={item.img} alt="Imagen del producto" />
+            <h3>{item.name}</h3>
+            <p>{item.detail}</p>
+            <p>$ {item.price}</p>
+          </div>
+        ))}
+      </div>
+      <div className="buyer-form">
+        <form>
+          <input
+            type="text"
+            value={buyer.name}
+            onChange={handleChange}
+            name="name"
+            placeholder="Nombre"
+          />
+          <input
+            type="text"
+            value={buyer.phone}
+            onChange={handleChange}
+            name="phone"
+            placeholder="Teléfono"
+          />
+          <input
+            type="email"
+            value={buyer.email}
+            onChange={handleChange}
+            name="email"
+            placeholder="email"
+          />
+        </form>
+      </div>
+      <div className="cart-control">
+        <button onClick={handleSendOrder}>Comprar</button>
+        <button onClick={clear}>Vaciar Carrito</button>
+      </div>
     </div>
   );
 };
